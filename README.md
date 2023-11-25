b0cd48c10d7868ee293092986eadf0aaf9da1fc2
CanAu21 <furkan.adrian9@outlook.com>
2023-11-25 09:25:05 +0300

    First portfolio website created

---
+++ README.md
@@ -0,0 +1,41 @@
+# Portfolio Website
+
+This project is a React application designed for showcasing your personal portfolio.
+
+## About the Project
+
+This project is created for a frontend developer to build a personal portfolio website. The project includes basic pages such as the home page, about page, and contact page. Additionally, it contains components necessary to customize the project according to your needs.
+
+## Installation
+
+1. Clone the project repository to your local machine:
+
+   ```bash
+   git clone https://github.com/your-username/portfolio-site.git
+   ```
+
+-Navigate to the project folder:
+cd portfolio
+
+-Install dependencies:
+npm install
+
+# Usage
+-After installing the project dependencies, start the development server:
+npm start
+
+The application will run by default at http://localhost:3000. Open this address in your browser to view the project.
+
+# Technologies
+This project uses the following technologies:
+React
+React Router (for page navigation)
+
+# License
+This project is licensed under the MIT License. See the license file for details.
+
+
+<img src="./src/assets/images/Front End Developer _ Freelancer - Google Chrome 2023-11-25 08-55-13 (online-video-cutter.com).gif" alt="" />
+
+
+
---
+++ src/components/Home/index.jsx
@@ -0,0 +1,71 @@
+import './index.scss'
+import LogoTitle from '../../assets/images/F harfi_transparent.png'
+import { Link } from 'react-router-dom'
+import {  useEffect, useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+import Logo from './Logo'
+import Loader from 'react-loaders'
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['F', 'u', 'r', 'k', 'a', 'n']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+
+  useEffect(() => {
+     setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+
+
+  return (
+    <>
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span id='char' className={`${letterClass} _14`}>'m</span>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
+        </h1>
+        <h2>Frontend Developer / React Expert</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+
+      <Logo/>
+    </div>
+    <Loader type='pacman'/>
+    </>
+  )
+}
+
+export default Home
